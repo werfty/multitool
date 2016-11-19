085db255b01a20ce3b53a180833944023a5cce03
Stefan Sels <github@tronicum.com>
2016-11-19 01:04:18 +0100

    formatting^

--- werfty-mt.go
+++ werfty-mt.go
@@ -8,28 +8,26 @@ import "os"
 import "net"
 
 func main() {
-connectivy_check()
+	connectivy_check()
 }
 
 func connectivy_check() {
-pinger, err := ping.NewPinger("www.google.com")
-if err != nil {
-        fmt.Println("icmp ping failed");
-	os.Exit(1)
-}
-pinger.Count = 3
-pinger.Run() // blocks until finished
-//stats := pinger.Statistics() // get send/receive/rtt stats
-//fmt.Println(stats)
+	pinger, err := ping.NewPinger("www.google.com")
+	if err != nil {
+		fmt.Println("icmp ping failed")
+		os.Exit(1)
+	}
+	pinger.Count = 3
+	pinger.Run() // blocks until finished
+	//stats := pinger.Statistics() // get send/receive/rtt stats
+	//fmt.Println(stats)
 
-conn, err := net.Dial("tcp", "google.com:80")
-if err != nil {
-	fmt.Println("error opening google.com tcp port 80")
-	os.Exit(1)
-} else {
-fmt.Fprintf(conn, "HEAD / HTTP/1.0\r\n\r\n")
-conn.Close()
+	conn, err := net.Dial("tcp", "google.com:80")
+	if err != nil {
+		fmt.Println("error opening google.com tcp port 80")
+		os.Exit(1)
+	} else {
+		fmt.Fprintf(conn, "HEAD / HTTP/1.0\r\n\r\n")
+		conn.Close()
+	}
 }
-}
-
-
